78ad4b6d9d9184510bb582cfd8401adcb0b82404
Zonglin-Li233 <zonglinl@uchicago.edu>
2026-05-23 18:08:00 -0500

    Add files via upload
    adapters init updated

--- src/quant_audit_benchmark/adapters/__init__.py
+++ src/quant_audit_benchmark/adapters/__init__.py
@@ -3,11 +3,13 @@
 
 from .base import ReviewResult, ReviewerAdapter
 from .corax_live import CoraxLiveAdapter
+from .darf_live import DarfLiveAdapter
 from .registry import ADAPTER_NAMES, DEFAULT_ADAPTER_NAMES, build_adapter
 
 __all__ = [
     "ADAPTER_NAMES",
     "CoraxLiveAdapter",
+    "DarfLiveAdapter",
     "DEFAULT_ADAPTER_NAMES",
     "ReviewResult",
     "ReviewerAdapter",
